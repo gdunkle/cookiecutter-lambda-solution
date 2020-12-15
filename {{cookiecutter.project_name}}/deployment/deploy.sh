2a126d90e79212115c5de065a70bafb1efc8e2b4
Galen Dunkleberger <awsgalen@amazon.com>
2020-12-15 13:27:30 -0500

    fix the deploy.sh to use a better name for the solutions s3 directory

--- {{cookiecutter.project_name}}/deployment/deploy.sh
+++ {{cookiecutter.project_name}}/deployment/deploy.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 export ACCOUNT_NAME=${ACCOUNT_NAME:={{cookiecutter.aws_account_name}}}
 export SOLUTION_NAME={{cookiecutter.project_name}}
-export DIST_OUTPUT_BUCKET=${DIST_OUTPUT_BUCKET:={{cookiecutter.author_prefix}}-solutions}
-export VERSION=1.0.0
 export AWS_REGION=${AWS_REGION:={{cookiecutter.aws_region}}}
+export DIST_OUTPUT_BUCKET=${DIST_OUTPUT_BUCKET:=$ACCOUNT_NAME-solutions-$AWS_REGION}
+export VERSION=1.0.0
+
 echo $SOLUTION_NAME 
 echo $DIST_OUTPUT_BUCKET
 echo $VERSION
 echo $AWS_REGION
 
 ./build-s3-dist.sh $DIST_OUTPUT_BUCKET $SOLUTION_NAME $VERSION
-aws s3 cp ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$AWS_REGION/$SOLUTION_NAME/$VERSION/ --recursive --acl bucket-owner-full-control
+aws s3 cp ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET/$SOLUTION_NAME/$VERSION/ --recursive --acl bucket-owner-full-control
 parameter_file=./parameters/$ACCOUNT_NAME-$AWS_REGION.json
 echo "jq -r '.[] | [.ParameterKey, .ParameterValue] | join(\"=\")' $parameter_file"
 parameter_overrides=$(jq -r '.[] | [.ParameterKey, .ParameterValue] | join("=")' $parameter_file)
 echo $parameter_overrides
 echo "Deploying stack $SOLUTION_NAME"
-echo "aws cloudformation deploy  --stack-name $SOLUTION_NAME --s3-bucket $DIST_OUTPUT_BUCKET-$AWS_REGION --s3-prefix $SOLUTION_NAME/$VERSION --template-file ./global-s3-assets/solution.template  --capabilities CAPABILITY_IAM"
-aws cloudformation deploy --region $AWS_REGION --parameter-overrides $parameter_overrides   --stack-name $SOLUTION_NAME --s3-bucket $DIST_OUTPUT_BUCKET-$AWS_REGION --s3-prefix $SOLUTION_NAME/$VERSION --template-file ./global-s3-assets/solution.template  --capabilities CAPABILITY_IAM
+echo "aws cloudformation deploy  --stack-name $SOLUTION_NAME --s3-bucket $DIST_OUTPUT_BUCKET --s3-prefix $SOLUTION_NAME/$VERSION --template-file ./global-s3-assets/solution.template  --capabilities CAPABILITY_IAM"
+aws cloudformation deploy --region $AWS_REGION --parameter-overrides $parameter_overrides   --stack-name $SOLUTION_NAME --s3-bucket $DIST_OUTPUT_BUCKET --s3-prefix $SOLUTION_NAME/$VERSION --template-file ./global-s3-assets/solution.template  --capabilities CAPABILITY_IAM
     
 
